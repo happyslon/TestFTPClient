bfa22fea5a291eb8a344143926e8370fe09268c7
happyslon <happyslon@mail.ru>
2018-09-05 14:43:37 +0300

    2

--- app/src/main/java/com/example/testftpclient/MainActivity.java
+++ app/src/main/java/com/example/testftpclient/MainActivity.java
@@ -3,35 +3,81 @@ package com.example.testftpclient;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.adeel.library.easyFTP;
 
 import java.io.InputStream;
+
+import static com.example.testftpclient.R.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+        Button btnStart = findViewById(id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new UploadTask().execute();
+            }
+        });
     }
 
-    class uploadTask extends AsyncTask<String, Void, String> {
+    class UploadTask extends AsyncTask<Void, String, Void> {
         @Override
-        protected String doInBackground(String... params) {
+        protected Void doInBackground(Void... params) {
             try {
-                easyFTP ftp = new easyFTP();
-                ftp.connect("address","username","password");
-                boolean status=false;
-                status=ftp.setWorkingDirectory("Files/Uploads/Images"); // if User say provided any Destination then Set it , otherwise
-                // Upload will be stored on Default /root level on server
-                InputStream is=getResources().openRawResource(+R.drawable.easyftptest);//??
-                ftp.uploadFile(is,"test.png");
-                return new String("Upload Successful");
+//                FTPClient mFtpClient = new FTPClient();
+//                mFtpClient.setConnectTimeout(10 * 1000);
+//                mFtpClient.connect(InetAddress.getByName(ip));
+//                status = mFtpClient.login(userName, pass);
+//                Log.e("isFTPConnected", String.valueOf(status));
+//
+//                FTPClient f = new FTPClient();
+//                f.connect(server);
+//                f.login(username, password);
+//                easyFTP ftp = new easyFTP();
+//                ftp.connect("188.247.50.242","","");
+//                boolean status=false;
+//                status=ftp.setWorkingDirectory("exchange_terminal/Holzunova15/"); // if User say provided any Destination then Set it , otherwise
+//                // Upload will be stored on Default /root level on server
+//                //InputStream is=getResources().openRawResource(drawable.easyftptest);//??
+
+//                easyFTP ftp = new easyFTP();
+//
+//                ftp.connect("188.247.50.242","","");
+//                String path = getFilesDir() + "/";
+//                ftp.downloadFile( "exchange_terminal/Holzunova15/1CtoShop.xml" , path+"1CtoShop.xml" );
+
+
+
+                //ftp.uploadFile(path+"1CtiShop1.xml");
+                publishProgress("Upload Successful");
+
+
             }catch (Exception e){
-                String t="Failure : " + e.getLocalizedMessage();
-                return t;
+                publishProgress("Failure : " + e.toString());
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            if(values != null && values.length != 0){
+                addLog(values[0]);
             }
         }
+
+    }
+
+    private void addLog(String values) {
+        Toast.makeText(this, values, Toast.LENGTH_SHORT).show();
     }
 }
