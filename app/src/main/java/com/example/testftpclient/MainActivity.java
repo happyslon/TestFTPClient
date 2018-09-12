2fbf91f2e12b8415391e74fd85e6413dcd41bc88
happyslon <happyslon@mail.ru>
2018-09-12 08:50:12 +0300

    downloadSingleFile

--- app/src/main/java/com/example/testftpclient/MainActivity.java
+++ app/src/main/java/com/example/testftpclient/MainActivity.java
@@ -9,11 +9,24 @@ import android.widget.Button;
 import android.widget.Toast;
 
 
-import java.io.InputStream;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import static com.example.testftpclient.R.*;
 
 public class MainActivity extends AppCompatActivity {
+    FTPClient mFtpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +45,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             try {
-
-                publishProgress("Upload Successful");
+                if(connectingwithFTP("188.247.50.242","root","zhbr159753")) {
+                    publishProgress("Connected");
+                }
+                File file = new File(getFilesDir() + "/"+"1CtoShop.xml");
+                if(downloadSingleFile(mFtpClient, "/exchange_terminal/Holzunova15/1CtoShop.xml", file)){
+                    publishProgress("downloaded file");
+                }
 
 
             }catch (Exception e){
@@ -55,5 +73,65 @@ public class MainActivity extends AppCompatActivity {
 
     private void addLog(String values) {
         Toast.makeText(this, values, Toast.LENGTH_SHORT).show();
+    }
+    /**
+     *
+     * @param ip
+     * @param userName
+     * @param pass
+     */
+    public boolean connectingwithFTP(String ip, String userName, String pass) {
+        boolean status = false;
+        try {
+            mFtpClient = new FTPClient();
+            mFtpClient.setConnectTimeout(10 * 1000);
+            mFtpClient.connect(InetAddress.getByName(ip));
+            status = mFtpClient.login(userName, pass);
+            Log.e("isFTPConnected", String.valueOf(status));
+            if (FTPReply.isPositiveCompletion(mFtpClient.getReplyCode())) {
+                mFtpClient.setFileType(FTP.ASCII_FILE_TYPE);
+                mFtpClient.enterLocalPassiveMode();
+                FTPFile[] mFileArray = mFtpClient.listFiles();
+                Log.e("Size", String.valueOf(mFileArray.length));
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+    /**
+     * @param ftpClient FTPclient object
+     * @param remoteFilePath  FTP server file path
+     * @param downloadFile   local file path where you want to save after download
+     * @return status of downloaded file
+     */
+    public boolean downloadSingleFile(FTPClient ftpClient,
+                                      String remoteFilePath, File downloadFile) {
+        File parentDir = downloadFile.getParentFile();
+        if (!parentDir.exists())
+            parentDir.mkdir();
+        OutputStream outputStream = null;
+        try {
+            outputStream = new BufferedOutputStream(new FileOutputStream(
+                    downloadFile));
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+            return ftpClient.retrieveFile(remoteFilePath, outputStream);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
     }
 }
